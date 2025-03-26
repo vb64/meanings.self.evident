01a8f275220e406c63b4d74cf71e36863f07cba8
Vitaly Bogomolov <vit.sar68@gmail.com>
2025-03-26 16:32:50 +0400

    step

--- txt/txt2md/makemd.py
+++ txt/txt2md/makemd.py
@@ -14,14 +14,31 @@ SENTENCE_END = ".!?"
 TXT = {
   Course.InSearchOfMeaning: {
     'Season01': [
-      ("pinker", (("Евгений Голуб:", Speak.Golub), ("Павел Щелин:", Speak.Shchelin))),
+      ("pinker", {"Евгений Голуб:": Speak.Golub, "Павел Щелин:": Speak.Shchelin}),
     ]
   }
 }
 
 
+def proc_line(out, line, speakers):
+    for speaker in speakers:
+        if line.startswith(speaker):
+            out.write("**{}:**\n".format(speakers[speaker]))
+            line = line[len(speaker):]
+            break
+
+    out.write(line.strip())
+    out.write('\n')
+
+
 def proc_txt(in_file, out_file, speakers):
     print(in_file)
+    lines = open(in_file, "rt", encoding="utf-8").readlines()
+    out = open(out_file, "wt", encoding="utf-8")
+    for line in lines:
+        proc_line(out, line, speakers)
+
+    out.close()
 
 
 def in_search_of_meaning():
