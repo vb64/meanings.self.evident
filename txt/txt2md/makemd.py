e124040d7df9478731153952e225142dc905a21b
Vitaly Bogomolov <vit.sar68@gmail.com>
2025-03-26 16:08:03 +0400

    step

--- txt/txt2md/makemd.py
+++ txt/txt2md/makemd.py
@@ -1,5 +1,34 @@
+import os
+
+
+class Course:
+    InSearchOfMeaning = "InSearchOfMeaning"
+
+
+class Speak:
+    Shchelin = "П.Щелин"
+    Golub = "Е.Голуб"
+
+SENTENCE_END = ".!?"
+
+GOLUB_SHCHELIN = (Speak.Golub, Speak.Shchelin)
+SHCHELIN_GOLUB = (Speak.Shchelin, Speak.Golub)
+
+TXT = {
+  Course.InSearchOfMeaning: {
+    'Season01': [
+      ("pinker", GOLUB_SHCHELIN),
+    ]
+  }
+}
+
+
 def in_search_of_meaning():
-    print("!!!")
+    data = TXT[Course.InSearchOfMeaning]
+    for season in data:
+        path = os.path.join('..', Course.InSearchOfMeaning, season)
+        for name, speakers in data[season]:
+            print(os.path.join(path, name + ".txt"))
 
 
 def main():
