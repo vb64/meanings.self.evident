406ef392f09540c35e0c63a727233fd34f8f25e3
Vitaly Bogomolov <vit.sar68@gmail.com>
2025-05-29 15:56:20 +0400

    step

--- scripts/make_srt.py
+++ scripts/make_srt.py
@@ -5,158 +5,136 @@ SPEAKER = len("Speaker 0: ")
 SRT_PATH = os.path.join('..', 'srt')
 
 GOLUB = (Speak.Golub, Speak.Shchelin)
+ROMANENKO = (Speak.Romanenko, Speak.Shchelin)
+SHELEST = (Speak.Shelest, Speak.Shchelin)
+PANCHENKO = (Speak.Panchenko, Speak.Shchelin)
+DUDNIK = (Speak.Dudnik, Speak.Shchelin)
+CHERNOV = (Speak.Chernov, Speak.Shchelin)
+BOBYLEV = (Speak.Bobileff, Speak.Shchelin)
+ARESTOVICH = (Speak.Arestovich, Speak.Shchelin)
+KOTAR = (Speak.Kotar, Speak.Shchelin)
 
-ROMANENKO_SHCHELIN = (Speak.Romanenko, Speak.Shchelin)
-SHCHELIN_ROMANENKO = (Speak.Shchelin, Speak.Romanenko)
-
-SHCHELIN_IZOTOV_ORLOV = (Speak.Shchelin, Speak.Izotov, Speak.OrlovSm)
-ORLOV_SHCHELIN_IZOTOV = (Speak.OrlovSm, Speak.Shchelin, Speak.Izotov)
-
-SHELEST_SHCHELIN = (Speak.Shelest, Speak.Shchelin)
-SHCHELIN_SHELEST = (Speak.Shchelin, Speak.Shelest)
-SHCHELIN_BONDARENKO_SHELEST = (Speak.Shchelin, Speak.Bondarenko, Speak.Shelest)
-
-SHCHELIN_PANCHENKO = (Speak.Shchelin, Speak.Panchenko)
-PANCHENKO_SHCHELIN = (Speak.Panchenko, Speak.Shchelin)
-
-SHCHELIN_TRANS_PANCHENKO = (Speak.Shchelin, Speak.Translator, Speak.Panchenko)
-SHCHELIN_PANCHENKO_TRANS = (Speak.Shchelin, Speak.Panchenko, Speak.Translator)
-
-SHCHELIN_DUDNIK = (Speak.Shchelin, Speak.Dudnik)
-SHCHELIN_DUDNIK_TRANS = (Speak.Shchelin, Speak.Dudnik, Speak.Translator)
-DUDNIK_SHCHELIN = (Speak.Dudnik, Speak.Shchelin)
-KHAZIN_SHCHELIN_DUDNIK = (Speak.Khazin, Speak.Shchelin, Speak.Dudnik)
-
-SHCHELIN_CHERNOV = (Speak.Shchelin, Speak.Chernov)
-CHERNOV_SHCHELIN = (Speak.Chernov, Speak.Shchelin)
-
-SHCHELIN_ROHLIN = (Speak.ShchelinEn, Speak.Rohlin)
-ROHLIN_SHCHELIN = (Speak.Rohlin, Speak.ShchelinEn)
-SHCHELIN_KOTAR = (Speak.Kotar, Speak.Shchelin)
-SHCHELIN_PAGEAU = (Speak.ShchelinEn, Speak.Pageau)
-PAGEAU_SHCHELIN = (Speak.Pageau, Speak.ShchelinEn)
-SHCHELIN_HALLJ = (Speak.Hall_J, Speak.ShchelinEn)
-SHCHELIN_HEERSJ = (Speak.ShchelinEn, Speak.Heers_J)
-SHCHELIN_ARESTOVICH = (Speak.Shchelin, Speak.Arestovich)
-SHCHELIN_BOBYLEV = (Speak.Bobileff, Speak.Shchelin)
-BOBYLEV_SHCHELIN = (Speak.Shchelin, Speak.Bobileff)
+ROHLIN = (Speak.Rohlin, Speak.ShchelinEn)
+PAGEAU = (Speak.Pageau, Speak.ShchelinEn)
+HALLJ = (Speak.Hall_J, Speak.ShchelinEn)
+HEERSJ = (Speak.Heers_J, Speak.ShchelinEn)
 
 SRT = {
 
   Course.Usa: [
-    ('2022_01_06', SHCHELIN_ARESTOVICH),
-    ('2022_01_10', ROMANENKO_SHCHELIN),
-    ('2022_01_14', SHCHELIN_ROMANENKO),
+    ('2022_01_06', reversed(ARESTOVICH)),
+    ('2022_01_10', ROMANENKO),
+    ('2022_01_14', reversed(ROMANENKO)),
   ],
 
   Course.English: [
-    ('2024_05_04', ROHLIN_SHCHELIN),
-    ('2024_05_31', SHCHELIN_KOTAR),
-    ('2024_06_12', SHCHELIN_ROHLIN),
-    ('2024_06_22', SHCHELIN_PAGEAU),
-    ('2024_09_17', PAGEAU_SHCHELIN),
-    ('2024_10_14', SHCHELIN_KOTAR),
-    ('2025_01_20', SHCHELIN_HALLJ),
-    ('2025_03_21', SHCHELIN_HEERSJ),
+    ('2024_05_04', ROHLIN),
+    ('2024_05_31', KOTAR),
+    ('2024_06_12', reversed(ROHLIN)),
+    ('2024_06_22', reversed(PAGEAU)),
+    ('2024_09_17', PAGEAU),
+    ('2024_10_14', KOTAR),
+    ('2025_01_20', HALLJ),
+    ('2025_03_21', reversed(HEERSJ)),
   ],
 
   Course.Mash: [
-    ("2025_03_28", SHCHELIN_IZOTOV_ORLOV),
-    ('2025_05_25', ORLOV_SHCHELIN_IZOTOV),
+    ("2025_03_28", (Speak.Shchelin, Speak.Izotov, Speak.OrlovSm)),
+    ('2025_05_25', (Speak.OrlovSm, Speak.Shchelin, Speak.Izotov)),
   ],
 
   Course.Chernov: [
-    ("2023_12_16", SHCHELIN_CHERNOV),
-    ("2024_04_16", CHERNOV_SHCHELIN),
-    ("2024_04_23", SHCHELIN_CHERNOV),
-    ("2024_05_22", SHCHELIN_CHERNOV),
-    ("2024_08_13", SHCHELIN_CHERNOV),
-    ("2025_04_07", SHCHELIN_CHERNOV),
+    ("2023_12_16", reversed(CHERNOV)),
+    ("2024_04_16", CHERNOV),
+    ("2024_04_23", reversed(CHERNOV)),
+    ("2024_05_22", reversed(CHERNOV)),
+    ("2024_08_13", reversed(CHERNOV)),
+    ("2025_04_07", reversed(CHERNOV)),
   ],
 
   Course.Panchenko: [
-    ("2024_06_22", SHCHELIN_PANCHENKO),
-    ("2024_11_06", SHCHELIN_PANCHENKO),
-    ("2025_01_23", SHCHELIN_PANCHENKO_TRANS),
-    ("2025_02_24", SHCHELIN_TRANS_PANCHENKO),
-    ("2025_04_06", SHCHELIN_PANCHENKO),
-    ("2025_05_16", SHCHELIN_PANCHENKO),
+    ("2024_06_22", reversed(PANCHENKO)),
+    ("2024_11_06", reversed(PANCHENKO)),
+    ("2025_01_23", (Speak.Shchelin, Speak.Panchenko, Speak.Translator)),
+    ("2025_02_24", (Speak.Shchelin, Speak.Translator, Speak.Panchenko)),
+    ("2025_04_06", reversed(PANCHENKO)),
+    ("2025_05_16", reversed(PANCHENKO)),
   ],
 
   Course.Dudnik: [
-    ("2022_11_03", SHCHELIN_DUDNIK),
-    ("2024_05_14", SHCHELIN_DUDNIK),
-    ("2024_06_04", SHCHELIN_DUDNIK),
-    ("2024_07_09", SHCHELIN_DUDNIK),
-    ("2024_08_14", SHCHELIN_DUDNIK_TRANS),
-    ("2024_09_13", SHCHELIN_DUDNIK),
-    ("2024_10_02", SHCHELIN_DUDNIK),
-    ("2024_10_14", SHCHELIN_DUDNIK),
-    ("2024_11_04", SHCHELIN_DUDNIK),
-    ("2024_11_23", SHCHELIN_DUDNIK),
-    ("2024_12_26", SHCHELIN_DUDNIK),
-    ("2025_01_15", DUDNIK_SHCHELIN),
-    ("2025_02_19", SHCHELIN_DUDNIK),
-    ("2025_03_11", SHCHELIN_DUDNIK),
-    ("2025_03_22", SHCHELIN_DUDNIK),
-    ("2025_03_25", KHAZIN_SHCHELIN_DUDNIK),
-    ("2025_04_24", SHCHELIN_DUDNIK),
-    ("2025_05_23", SHCHELIN_DUDNIK),
+    ("2022_11_03", reversed(DUDNIK)),
+    ("2024_05_14", reversed(DUDNIK)),
+    ("2024_06_04", reversed(DUDNIK)),
+    ("2024_07_09", reversed(DUDNIK)),
+    ("2024_08_14", (Speak.Shchelin, Speak.Dudnik, Speak.Ads)),
+    ("2024_09_13", reversed(DUDNIK)),
+    ("2024_10_02", reversed(DUDNIK)),
+    ("2024_10_14", reversed(DUDNIK)),
+    ("2024_11_04", reversed(DUDNIK)),
+    ("2024_11_23", reversed(DUDNIK)),
+    ("2024_12_26", reversed(DUDNIK)),
+    ("2025_01_15", DUDNIK),
+    ("2025_02_19", reversed(DUDNIK)),
+    ("2025_03_11", reversed(DUDNIK)),
+    ("2025_03_22", reversed(DUDNIK)),
+    ("2025_03_25", (Speak.Khazin, Speak.Shchelin, Speak.Dudnik)),
+    ("2025_04_24", reversed(DUDNIK)),
+    ("2025_05_23", reversed(DUDNIK)),
   ],
 
   Course.Shelest: [
-    ("2023_02_09", SHCHELIN_SHELEST),
-    ("2023_04_01", SHCHELIN_SHELEST),
-    ("2023_05_04", SHCHELIN_SHELEST),
-    ("2023_06_21", SHCHELIN_SHELEST),
-    ("2023_07_15", SHCHELIN_SHELEST),
-    ("2023_08_03", SHCHELIN_SHELEST),
-    ("2023_08_30", SHCHELIN_SHELEST),
-    ("2023_09_23", SHCHELIN_SHELEST),
-    ("2023_10_12", SHELEST_SHCHELIN),
-    ("2023_11_02", SHELEST_SHCHELIN),
-    ("2023_11_22", SHELEST_SHCHELIN),
-    ("2023_11_23", SHELEST_SHCHELIN),
-    ("2023_12_13", SHCHELIN_SHELEST),
-    ("2023_12_28", SHELEST_SHCHELIN),
-    ("2024_01_19", SHELEST_SHCHELIN),
-    ("2024_02_10", SHCHELIN_SHELEST),
-    ("2024_03_14", SHCHELIN_SHELEST),
-    ("2024_04_03", SHCHELIN_SHELEST),
-    ("2024_04_24", SHCHELIN_SHELEST),
-    ("2024_05_10", SHCHELIN_SHELEST),
-    ("2024_05_29", SHELEST_SHCHELIN),
-    ("2024_06_15", SHCHELIN_SHELEST),
-    ("2024_06_26", SHELEST_SHCHELIN),
-    ("2024_08_07", SHCHELIN_SHELEST),
-    ("2024_08_19", SHCHELIN_SHELEST),
-    ("2024_09_04", SHCHELIN_SHELEST),
-    ("2024_09_26", SHELEST_SHCHELIN),
-    ("2024_10_10", SHELEST_SHCHELIN),
-    ("2024_10_25", SHELEST_SHCHELIN),
-    ("2024_11_12", SHCHELIN_SHELEST),
-    ("2024_11_30", SHCHELIN_SHELEST),
-    ("2024_12_10", SHCHELIN_SHELEST),
-    ("2024_12_24", SHELEST_SHCHELIN),
-    ("2025_01_11", SHELEST_SHCHELIN),
-    ("2025_01_27", SHELEST_SHCHELIN),
-    ("2025_02_11", SHELEST_SHCHELIN),
-    ("2025_02_22", SHELEST_SHCHELIN),
-    ("2025_03_06", SHCHELIN_SHELEST),
-    ("2025_03_17", SHCHELIN_SHELEST),
-    ("2025_03_31", SHCHELIN_BONDARENKO_SHELEST),
-    ("2025_04_09", SHELEST_SHCHELIN),
-    ("2025_04_21", SHCHELIN_SHELEST),
-    ("2025_05_12", SHCHELIN_SHELEST),
-    ('2025_05_27', SHCHELIN_SHELEST),
+    ("2023_02_09", reversed(SHELEST)),
+    ("2023_04_01", reversed(SHELEST)),
+    ("2023_05_04", reversed(SHELEST)),
+    ("2023_06_21", reversed(SHELEST)),
+    ("2023_07_15", reversed(SHELEST)),
+    ("2023_08_03", reversed(SHELEST)),
+    ("2023_08_30", reversed(SHELEST)),
+    ("2023_09_23", reversed(SHELEST)),
+    ("2023_10_12", SHELEST),
+    ("2023_11_02", SHELEST),
+    ("2023_11_22", SHELEST),
+    ("2023_11_23", SHELEST),
+    ("2023_12_13", reversed(SHELEST)),
+    ("2023_12_28", SHELEST),
+    ("2024_01_19", SHELEST),
+    ("2024_02_10", reversed(SHELEST)),
+    ("2024_03_14", reversed(SHELEST)),
+    ("2024_04_03", reversed(SHELEST)),
+    ("2024_04_24", reversed(SHELEST)),
+    ("2024_05_10", reversed(SHELEST)),
+    ("2024_05_29", SHELEST),
+    ("2024_06_15", reversed(SHELEST)),
+    ("2024_06_26", SHELEST),
+    ("2024_08_07", reversed(SHELEST)),
+    ("2024_08_19", reversed(SHELEST)),
+    ("2024_09_04", reversed(SHELEST)),
+    ("2024_09_26", SHELEST),
+    ("2024_10_10", SHELEST),
+    ("2024_10_25", SHELEST),
+    ("2024_11_12", reversed(SHELEST)),
+    ("2024_11_30", reversed(SHELEST)),
+    ("2024_12_10", reversed(SHELEST)),
+    ("2024_12_24", SHELEST),
+    ("2025_01_11", SHELEST),
+    ("2025_01_27", SHELEST),
+    ("2025_02_11", SHELEST),
+    ("2025_02_22", SHELEST),
+    ("2025_03_06", reversed(SHELEST)),
+    ("2025_03_17", reversed(SHELEST)),
+    ("2025_03_31", (Speak.Shchelin, Speak.Bondarenko, Speak.Shelest)),
+    ("2025_04_09", SHELEST),
+    ("2025_04_21", reversed(SHELEST)),
+    ("2025_05_12", reversed(SHELEST)),
+    ('2025_05_27', reversed(SHELEST)),
   ],
 
   Course.GnosticThinking: [
-    ("gnosticism", SHCHELIN_ROMANENKO),
-    ("modern", SHCHELIN_ROMANENKO),
-    ("gobs", SHCHELIN_ROMANENKO),
-    ("enlightenment", SHCHELIN_ROMANENKO),
-    ("nationalism", ROMANENKO_SHCHELIN),
+    ("gnosticism", reversed(ROMANENKO)),
+    ("modern", reversed(ROMANENKO)),
+    ("gobs", reversed(ROMANENKO)),
+    ("enlightenment", reversed(ROMANENKO)),
+    ("nationalism", ROMANENKO),
 
     ("marxism", reversed(GOLUB)),
     ("positivism", reversed(GOLUB)),
@@ -228,36 +206,36 @@ SRT = {
   ],
 
   Course.Bobileff: [
-    ('2022_09_10', SHCHELIN_BOBYLEV),
-    ('2022_09_12', SHCHELIN_BOBYLEV),
+    ('2022_09_10', BOBYLEV),
+    ('2022_09_12', BOBYLEV),
     ('2023_09_10', (Speak.Shchelin, Speak.Bobileff, Speak.Boglaev)),
-    ('2023_09_13', BOBYLEV_SHCHELIN),
+    ('2023_09_13', reversed(BOBYLEV)),
     ('2023_10_03', (Speak.Boglaev, Speak.Shchelin, Speak.Bobileff)),
     ('2023_10_12', (Speak.Bobileff, Speak.Shchelin, Speak.Boglaev)),
-    ('2023_10_15', SHCHELIN_BOBYLEV),
-    ('2023_10_18', SHCHELIN_BOBYLEV),
-    ('2023_12_01', SHCHELIN_BOBYLEV),
+    ('2023_10_15', BOBYLEV),
+    ('2023_10_18', BOBYLEV),
+    ('2023_12_01', BOBYLEV),
     ('2023_12_03', (Speak.Shchelin, Speak.Bobileff, Speak.Ads)),
-    ('2024_01_18', SHCHELIN_BOBYLEV),
-    ('2024_01_20', SHCHELIN_BOBYLEV),
-    ('2024_03_25', SHCHELIN_BOBYLEV),
-    ('2024_04_21', SHCHELIN_BOBYLEV),
-    ('2024_04_24', BOBYLEV_SHCHELIN),
-    ('2024_06_06', SHCHELIN_BOBYLEV),
-    ('2024_06_11', SHCHELIN_BOBYLEV),
-    ('2024_07_13', BOBYLEV_SHCHELIN),
-    ('2024_07_15', SHCHELIN_BOBYLEV),
+    ('2024_01_18', BOBYLEV),
+    ('2024_01_20', BOBYLEV),
+    ('2024_03_25', BOBYLEV),
+    ('2024_04_21', BOBYLEV),
+    ('2024_04_24', reversed(BOBYLEV)),
+    ('2024_06_06', BOBYLEV),
+    ('2024_06_11', BOBYLEV),
+    ('2024_07_13', reversed(BOBYLEV)),
+    ('2024_07_15', BOBYLEV),
     ('2024_10_05', (Speak.Burov, Speak.Shchelin, Speak.Bobileff)),
     ('2024_10_12', (Speak.Bobileff, Speak.Burov, Speak.Shchelin)),
-    ('2024_11_20', BOBYLEV_SHCHELIN),
-    ('2024_11_21', BOBYLEV_SHCHELIN),
-    ('2025_01_12', SHCHELIN_BOBYLEV),
-    ('2025_01_14', BOBYLEV_SHCHELIN),
-    ('2025_02_03', BOBYLEV_SHCHELIN),
-    ('2025_03_25', SHCHELIN_BOBYLEV),
-    ('2025_03_26', BOBYLEV_SHCHELIN),
-    ('2025_04_25', SHCHELIN_BOBYLEV),
-    ('2025_04_28', SHCHELIN_BOBYLEV),
+    ('2024_11_20', reversed(BOBYLEV)),
+    ('2024_11_21', reversed(BOBYLEV)),
+    ('2025_01_12', BOBYLEV),
+    ('2025_01_14', reversed(BOBYLEV)),
+    ('2025_02_03', reversed(BOBYLEV)),
+    ('2025_03_25', BOBYLEV),
+    ('2025_03_26', reversed(BOBYLEV)),
+    ('2025_04_25', BOBYLEV),
+    ('2025_04_28', BOBYLEV),
   ],
 
 }
