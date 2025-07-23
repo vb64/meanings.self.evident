82361c9638344c26233f93a217505620eb81b2ac
Vitaly Bogomolov <vit.sar68@gmail.com>
2025-07-23 10:07:16 +0400

    Радио "Комсомольская правда". (#179)

--- scripts/text.py
+++ scripts/text.py
@@ -62,6 +62,7 @@ class Speak:
     Kniazev = "И.Князев"
     Sobolev = "Н.Соболев"
     DSmirnov = "Д.Смирнов"
+    IPankin = "И.Панкин"
 
     ShchelinEn = "P.Shchelin"
     Rohlin = "R.Rohlin"
@@ -305,6 +306,7 @@ COURSE_SINGLES = [
   ('2022_08_27', False, (Speak.Svetov, Speak.Shchelin)),
   ('2025_05_08', False, (Speak.Shchelin, Speak.Klizma)),
   ('2025_06_30', False, (Speak.Shchelin, Speak.DSmirnov)),
+  ('2025_07_22', False, (Speak.Shchelin, Speak.IPankin)),
 ]
 COURSE_URALOV = [
   ('2024_12_25', False, (Speak.Uralov, Speak.Shchelin, Speak.Kniazev, Speak.Chadaev)),
