345904ff3a340e88d6fa82290d2701a51525eb38
vb64 <vit.sar68@gmail.com>
2025-07-27 23:06:27 +0400

    step

--- scripts/text.py
+++ scripts/text.py
@@ -267,7 +267,7 @@ COURSE_BOBYLEV = [
   ('2025_04_25', False, BOBYLEV),
   ('2025_04_28', False, BOBYLEV),
   ('2025_07_25', False, (Speak.Shchelin, Speak.Bobileff, Speak.Bobileff)),
-  ('2025_07_27', False, BOBYLEV),
+  ('2025_07_27', False, (Speak.Shchelin, Speak.Bobileff, Speak.Bobileff)),
 ]
 KHIMICH = (Speak.Khimich, Speak.Shchelin)
 COURSE_KHIMICH = [
