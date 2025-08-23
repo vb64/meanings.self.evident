abc2d685757dad7e6289223505dd44e72027d24e
Vitaly Bogomolov <vit.sar68@gmail.com>
2025-08-23 20:30:10 +0400

    Mnenie 23.08.2025 (#189)

---
+++ scripts/make_transcript.py
@@ -0,0 +1,58 @@
+import os
+from text import Course, Speak, split_to_sentences
+
+SPEAKER_MARK = "SPK_"
+TXT_PATH = os.path.join('..', 'txt')
+TXT = {
+  Course.Mnenie: [
+      ("2025_08_23", (Speak.Askerhanov, Speak.Shchelin)),
+  ],
+}
+
+
+def proc_line(speaker_index, out, line, speakers):
+    if SPEAKER_MARK in line:
+        index = int(line.split(SPEAKER_MARK)[1]) - 1
+        if speaker_index != index:
+            out.write('\n')
+            out.write("**{}:**\n".format(speakers[index]))
+        return index
+
+    for sentence in split_to_sentences(line):
+        out.write(sentence)
+        out.write('\n')
+
+    return speaker_index
+
+
+def proc_txt(in_file, out_file, speakers):
+    print(in_file)
+    lines = open(in_file, "rt", encoding="utf-8").readlines()
+    out = open(out_file, "wt", encoding="utf-8")
+    speaker_index = -1
+    for line in lines:
+        speaker_index = proc_line(speaker_index, out, line.strip(), speakers)
+
+    out.close()
+
+
+def podcast(folder):
+    data = TXT[folder]
+    path = os.path.join(TXT_PATH, folder)
+    out_path = os.path.join("build", folder)
+    os.makedirs(out_path, exist_ok=True)
+
+    for name, speakers in data:
+        proc_txt(
+          os.path.join(path, name + ".txt"),
+          os.path.join(out_path, name + ".md"),
+          speakers
+        )
+
+
+def main():
+    podcast(Course.Mnenie)
+
+
+if __name__ == '__main__':
+    main()
