ae3c266c85a988a606a9fe2a711f7b5a78d22b32
Vitaly Bogomolov <vit.sar68@gmail.com>
2025-12-04 17:13:56 +0400

    step

---
+++ scripts/make_md.py
@@ -0,0 +1,41 @@
+import os
+from text import Course, Speak
+
+MD_PATH = os.path.join('..', 'md')
+MDATA = {
+  Course.Shelest: [
+      ("2025_12_01", (Speak.Shelest, Speak.Shchelin)),
+  ],
+}
+
+
+def proc_md(in_file, out_file, speakers):
+    print(in_file)
+    body = open(in_file, "rt", encoding="utf-8").read()
+    for i, speaker in enumerate(speakers):
+        body = body.replace("SPK_{}:".format(i), "{}:".format(speaker))
+
+    with open(out_file, "wt", encoding="utf-8") as out:
+        out.write(body)
+
+
+def podcast(folder):
+    data = MDATA[folder]
+    path = os.path.join(MD_PATH, folder)
+    out_path = os.path.join("build", folder)
+    os.makedirs(out_path, exist_ok=True)
+
+    for name, speakers in data:
+        proc_md(
+          os.path.join(path, name + ".md"),
+          os.path.join(out_path, name + ".md"),
+          speakers
+        )
+
+
+def main():
+    podcast(Course.Shelest)
+
+
+if __name__ == '__main__':
+    main()
