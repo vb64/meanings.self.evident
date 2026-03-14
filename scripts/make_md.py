a748277af5c12b353d2d458a38256aa4edeaff91
vb64 <vit.sar68@gmail.com>
2026-03-14 23:49:24 +0400

    step

--- scripts/make_md.py
+++ scripts/make_md.py
@@ -63,7 +63,7 @@ MDATA = {
       ("2026_01_04", (Speak.ATkachev, Speak.Shchelin)),
       ("2026_01_17", (Speak.ATkachev, Speak.Shchelin)),
       ("2026_02_14", (Speak.ATkachev, Speak.Shchelin)),
-      ("2026_03_13", (Speak.ATkachev, Speak.Shchelin)),
+      ("2026_03_13", (Speak.Shchelin, Speak.ATkachev)),
   ],
   Course.Sputnik: [
       ("2025_12_14", (Speak.Shchelin, Speak.POstrovsky, Speak.PUvarova)),
