3652b6519c05e6255748d67b440f811fa4345961
vb64 <vit.sar68@gmail.com>
2026-04-10 23:47:36 +0400

    Golub 06.04.2026

--- scripts/make_md.py
+++ scripts/make_md.py
@@ -13,6 +13,7 @@ MDATA = {
       ("Season06/the-only-questtion", (Speak.Golub, Speak.Shchelin)),
       ("Season06/memory", (Speak.Shchelin, Speak.Golub)),
       ("Season06/the-price-of-non-existance", (Speak.Shchelin, Speak.Golub)),
+      ("Season06/uprazdenenie-blagovremenie", (Speak.Golub, Speak.Shchelin)),
   ],
   Course.NamingTheSelfEvident: [
       ("2026_02_27", (Speak.ShchelinEn, Speak.Hall_J)),
