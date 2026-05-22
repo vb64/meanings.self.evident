f791fae174d9d56e26073ea5258a63affe4709e9
Vitaly Bogomolov <vit.sar68@gmail.com>
2026-05-22 14:05:10 +0400

    step

--- scripts/make_md.py
+++ scripts/make_md.py
@@ -43,6 +43,7 @@ MDATA = {
       ("2026_02_16", (Speak.Shchelin, Speak.Shelest)),
       ("2026_03_19", (Speak.Shchelin, Speak.Shelest)),
       ("2026_04_14", (Speak.Shchelin, Speak.Shelest)),
+      ("2026_05_22", (Speak.Shchelin, Speak.Shelest)),
   ],
   Course.Dudnik: [
       ("2025_12_19", (Speak.Shchelin, Speak.Dudnik)),
